f443a5c8d57c5956c901bd7f71023c0056bba3cf
kubapadzik <u73681@student.wat.edu.pl>
2026-04-16 12:41:55 +0200

    wip: simple structure od users list

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Geodezja i Geoinformatyka rządzi!')
+zmienna_1: str = 'Oliwia'
+zmienna_2: str = 'Paweł'
+zmienna_3: str = 'Eliza'
+zmienna_4: str = 'Filip'
+zmienna_5: str = 'Weronika'
+zmienna_6: str = 'Ewelina'
+
+print(zmienna_1, zmienna_2)
+print(zmienna_3)
+print(zmienna_4)
+print(zmienna_5)
+print(zmienna_6)
