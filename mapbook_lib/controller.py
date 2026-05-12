ecd05c75540748d603f72c9f5a3e730040c27bb8
kubapadzik <u73681@student.wat.edu.pl>
2026-05-12 16:01:35 +0200

    feat: add update funcion

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -18,3 +18,29 @@ def remove_user(users_data: list) -> None:
     for user in users_data:
         if user['username'] == name:
             users_data.remove(user)
+
+def update_user(users_data: list) ->None:
+    name=input('Podaj imię użytkownika do zmiany: ')
+
+    for user in users_data:
+        if user['username'] == name:
+            user['username']=input('Podaj nowe imie')
+            user['location']=input('Podaj nową loklaizację: ')
+            user['posts']=input('Podaj liczbę postów: ')
+    users_data(user)
+    print(user)
+
+def get_coordinates(location:str)->list:
+
+
+    url=f'https://pl.wikipedia.org/wiki/{location}'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+    response_html=BeautifulSoup(response.text, 'html.parser')
+    response_html_latitude=float(response_html.select('.latitude')[1].text.replace(',','.'))
+    response_html_longitude=float(response_html.select('.longitude')[1].text.replace(',','.'))
+    return[response_html_latitude,response_html_longitude]
+
+for user in users:
+    print(get_coordinates(location=user['location']))
